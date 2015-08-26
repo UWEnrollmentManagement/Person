b60501337d0f144c3ca4034a53ff8eb28ccca84f
Joseph Schilz <joseph@schilz.org>
2015-08-26 15:34:36 -0700

    Fix .travis.yml

--- tests/.bootstrap.php
+++ tests/.bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
+require_once dirname(__FILE__) ."/../src/Person.php";
 require_once dirname(__FILE__) ."/../src/Alumni.php";
 require_once dirname(__FILE__) ."/../src/Connection.php";
 require_once dirname(__FILE__) ."/../src/Employee.php";
 require_once dirname(__FILE__) ."/../src/Parser.php";
-require_once dirname(__FILE__) ."/../src/Person.php";
 require_once dirname(__FILE__) ."/../src/Student.php";
 require_once dirname(__FILE__) ."/mocks.php";
