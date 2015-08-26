4700a43d51968b5c226a254b43ff852392c41148
Joseph Schilz <joseph@schilz.org>
2015-08-26 15:33:22 -0700

    Fix .travis.yml

--- tests/.bootstrap.php
+++ tests/.bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) ."/../vendor/autoload.php";
-
 require_once dirname(__FILE__) ."/../src/Alumni.php";
 require_once dirname(__FILE__) ."/../src/Connection.php";
 require_once dirname(__FILE__) ."/../src/Employee.php";
