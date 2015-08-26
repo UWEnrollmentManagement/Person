322f4ac8ae38052bc2109a2f5dd012de965c886b
Joseph Schilz <joseph@schilz.org>
2015-08-26 15:39:39 -0700

    Fix .travis.yml

--- tests/.bootstrap.php
+++ tests/.bootstrap.php
@@ -7,4 +7,61 @@ require_once dirname(__FILE__) ."/../src/Employee.php";
 require_once dirname(__FILE__) ."/../src/Parser.php";
 require_once dirname(__FILE__) ."/../src/Student.php";
 
-require_once dirname(__FILE__) ."/../tests/mocks.php";
+
+use UWDOEM\Person\Person;
+use UWDOEM\Person\Student;
+use UWDOEM\Person\Employee;
+use UWDOEM\Person\Alumni;
+
+
+class MockConnectionInstance {
+
+    public $lastUrl;
+    public $lastParams;
+
+    protected function makeSlug($url) {
+        $url = str_replace(["https://ws.admin.washington.edu/identity/v1/"], [""], $url);
+        $url = str_replace(["?", "/", ".", "="], ["-", "-", "-", "-"], $url);
+
+        return $url;
+    }
+
+    public function execGET($url, $params = []) {
+        $this->lastUrl = $url;
+        $this->lastParams = $params;
+
+        return file_get_contents(getcwd() . "/tests/responses/{$this->makeSlug($url)}.json");
+    }
+
+    public function execPOST($url, $params = []) {
+        $this->lastUrl = $url;
+        $this->lastParams = $params;
+        return file_get_contents(getcwd() . "/tests/StaffStudentPerson.json");
+    }
+}
+
+$myMockConnectionInstance = new MockConnectionInstance();
+
+trait MockPersonTrait {
+    protected static function getConn() {
+        global $myMockConnectionInstance;
+        return $myMockConnectionInstance;
+    }
+}
+
+class MockPerson extends Person {
+    use MockPersonTrait;
+
+}
+
+class MockStudent extends Student {
+    use MockPersonTrait;
+}
+
+class MockEmployee extends Employee {
+    use MockPersonTrait;
+}
+
+class MockAlumni extends Alumni {
+    use MockPersonTrait;
+}
