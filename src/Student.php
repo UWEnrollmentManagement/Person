70f73604670c764ec6c0b2b411b730bab17c10c8
Joseph Schilz <joseph@schilz.org>
2015-11-23 08:08:35 -0800

    Provide registrationSearch.

--- src/Student.php
+++ src/Student.php
@@ -13,6 +13,53 @@ class Student extends Person {
 
     protected static $AFFILIATION_TYPE = "student";
 
+
+    /**
+     * Perform a registration search on SWS, per https://wiki.cac.washington.edu/display/SWS/Registration+Search+Resource+v5
+     *
+     * @param string|int $year numeric year between 1950 and 2100 inclusive
+     * @param string $quarter one of ["autumn", "winter", "spring", "summer"]
+     * @param string[] $extraSearchTerms associative array of search terms ex: ["course_number" => "100"]
+     * @return mixed associative array object of registration search results
+     * @throws \Exception if provided with invalid year, invalid quarter, or invalid search terms
+     */
+    public function registrationSearch($year, $quarter, array $extraSearchTerms = []) {
+
+        if (!is_numeric($year) || $year < 1950 || $year > 2100) {
+            throw new \Exception("Please provide a numeric year between 1950 and 2100");
+        }
+
+        $validQuarters = ["autumn", "winter", "spring", "summer"];
+        if (!in_array("$quarter", $validQuarters)) {
+            throw new \Exception("Quarter must be one of [" . implode(", $validQuarters") . "], case sensitive.");
+        }
+
+        $validSearchKeys = [
+            "curriculum_abbreviation", "course_number", "section_id", "instructor_reg_id",
+            "is_active", "verbose", "changed_since_date", "transcriptable_course", "grading_system"
+        ];
+        $invalidSearchKeys = array_diff(array_keys($extraSearchTerms), $validSearchKeys);
+        if ($invalidSearchKeys) {
+            throw new \Exception("Invalid search keys [" . implode(", ", $invalidSearchKeys) . "]" .
+            "provided as extra search terms. Only [" . implode(", ", $validSearchKeys) . "] allowed");
+        }
+
+        $defaultSearchTerms = [
+            "curriculum_abbreviation"=> "",
+            "course_number" => "",
+            "section_id" => "",
+        ];
+
+        $searchTerms = array_merge($defaultSearchTerms, $extraSearchTerms);
+        $searchTerms["reg_id"] = $this->getAttr("UWRegID");
+
+        $resp = static::getStudentConn()->execGET(
+            "registration.json?" . http_build_query($searchTerms)
+        );
+
+        return static::parse($resp);
+    }
+
     protected static function fromSimpleIdentifier($identifier) {
 
         $person = parent::fromSimpleIdentifier($identifier);
@@ -29,7 +76,6 @@ class Student extends Person {
 
         return $person;
     }
-
 
     protected static function fill(Person $person, array $attrs) {
 
